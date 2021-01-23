93e3b549b32731cfd363e317e41411e9667082bf
ratokeshi <roy.tokeshi@gmail.com>
2021-01-23 12:25:42 -0700

    check for backups and copy current file, and create backups

--- cal.py
+++ cal.py
@@ -1,8 +1,10 @@
 import datetime as dt
 import pandas as pd
 import win32com.client
+import fnmatch, sys
 import os.path
 from os import path 
+from shutil import copyfile
 
 def get_calendar(begin,end):
     outlook = win32com.client.Dispatch('Outlook.Application').GetNamespace('MAPI')
@@ -39,19 +41,55 @@ def make_cpd(appointments):
     summary = appointments.groupby('Meeting Description')['Hours'].sum()
     return summary
 
-def main():
+def findfile(lookfor):
+    lookfor="*" + lookfor + "*"
+    #print("...Searching the following files for pattern match: " + str(os.listdir(os.path.dirname(__file__))))
+    #print("...Looking at this list of files:")
+    #print(os.listdir(os.path.dirname(__file__)))
+    for file in os.listdir(os.path.dirname(__file__)):
+        print("...Compare: " + file + " to pattern: " + lookfor)
+        if fnmatch.fnmatch(file, lookfor):
+            print(lookfor + ". is in directory")
+            break
+            #return True
+        else:
+            print(lookfor + ". is NOT a match.")
+    return(file)        
+     
+#def main():
     
-   print ("File exists:" + str(path.exists('meeting hours.xlsx')))
-   print ("File exists:" + str(path.exists('meeting hours.xlsx')))
-   print ("directory exists:" + str(path.exists('myDirectory')))
+   #print ("File exists:" + str(path.exists('meeting hours.xlsx')))
+   #print ("File exists:" + str(path.exists('meeting hours.xlsx')))
+   #print ("directory exists:" + str(path.exists('myDirectory')))
 
-
+# Set beginning and ending meeting search range
 begin = dt.datetime(2021,1,17)
 end = dt.datetime(2021,1,23)
+filename="Meeting_Hours"
+filetype="xlsx"
+fileoutput=filename + "." + filetype
+
 
 cal = get_calendar(begin, end)
 appointments = get_appointments(cal, subject_kw = ' ', exclude_subject_kw = 'Canceled')
 result = make_cpd(appointments)
 
-if path.exists('meeting hours.xlsx')
-result.to_excel('meeting hours.xlsx')
+# Create a timestamp for backup files 
+now=dt.datetime.now()
+timestamp="-" + str(now.year) + "-" + str("%02d"%now.month) + "-" + str("%02d"%now.day) + "--" + str("%02d"%now.hour) + str("%02d"%now.minute) + str("%02d"%now.second)
+print("...Timestame is: " + timestamp)
+
+#look for backup files if none create 
+looking = findfile("backup")
+print("...Searching the following files for pattern match: " + str(os.listdir(os.path.dirname(__file__))))
+if findfile("backup"):
+    print("...Yes, there is a backup here. ")
+else:
+    copyfile(fileoutput, filename + timestamp + "-backup." + filetype)
+    print("...Creating first backup called: " + fileoutput, filename + timestamp + "-backup." + filetype)      
+if path.exists(fileoutput):
+    print("..." + fileoutput + " file Exists.")
+    os.rename(fileoutput, fileoutput + timestamp + "-backup." + filetype)
+    print("...Renamed file to: " + fileoutput + timestamp + "-backup." + filetype)
+result.to_excel(fileoutput)
+print("...New output file created: " + fileoutput)
