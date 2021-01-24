38605ebfbbfc1470cc173fb625661e3356c73add
ratokeshi <roy.tokeshi@gmail.com>
2021-01-23 23:26:49 -0700

    add recipients viewed as comm ojects

--- cal.py
+++ cal.py
@@ -26,19 +26,24 @@ def get_appointments(calendar,subject_kw = None,exclude_subject_kw = None, body_
     cal_start = [app.start for app in appointments]
     cal_end = [app.end for app in appointments]
     cal_body = [app.body for app in appointments]
+    cal_recipients = [app.recipients for app in appointments]
 
     df = pd.DataFrame({'subject': cal_subject,
                        'start': cal_start,
                        'end': cal_end,
-                       'body': cal_body})
+                       'body': cal_body,
+                       'recipients': cal_recipients})
+    df.to_csv('results.txt', index=False)                   
     return df
 
 def make_cpd(appointments):
     appointments['Date'] = appointments['start']
     appointments['Hours'] = (appointments['end'] - appointments['start']).dt.seconds/3600
-    appointments.rename(columns={'subject':'Meeting Description'}, inplace = True)
+    print(appointments)
+    appointments.rename(columns={'subject':'Meeting Description','recipients':'Recipients','body':'Body'}, inplace = True)
     appointments.drop(['start','end'], axis = 1, inplace = True)
-    summary = appointments.groupby('Meeting Description')['Hours'].sum()
+    print(appointments)
+    summary = appointments.groupby('Meeting Description')['Hours'].sum()#.get_group('Recipients')
     return summary
 
 def findfile(lookfor):
@@ -68,6 +73,8 @@ fileoutput=filename + "." + filetype
 cal = get_calendar(begin, end)
 appointments = get_appointments(cal, subject_kw = ' ', exclude_subject_kw = 'Canceled')
 result = make_cpd(appointments)
+print(result)
+
 
 # Create a timestamp for backup files 
 now=dt.datetime.now()
